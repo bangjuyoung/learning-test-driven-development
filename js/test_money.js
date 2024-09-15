29196ffd85e3d8df8354d345faeb5c4a4ca7f046
bangjuyoung <juyoungbang82@gmail.com>
2024-09-15 17:19:06 +0900

    feat(chap02): red test

--- js/test_money.js
+++ js/test_money.js
@@ -13,3 +13,8 @@ class Dollar {
 let fiver = new Dollar(10);
 let tenner = fiver.times(2);
 assert.strictEqual(tenner.amount, 20);
+
+let tenEuros = new Money(10, "EUR");
+let twentyEuros = tenEuros.times(2)
+assert.strictEqual(twentyEuros.amount, 20);
+assert.strictEqual(twentyEuros.currency, "EUR");
