a92976d831ece99ea9be3c7f7c19c9dd153e4c45
bangjuyoung <juyoungbang82@gmail.com>
2024-09-15 17:21:04 +0900

    feat(chap02): green test

--- js/test_money.js
+++ js/test_money.js
@@ -14,6 +14,17 @@ let fiver = new Dollar(10);
 let tenner = fiver.times(2);
 assert.strictEqual(tenner.amount, 20);
 
+class Money {
+    constructor(amount, currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    times(multiplier) {
+        return new Money(this.amount * multiplier, this.currency);
+    }
+}
+
 let tenEuros = new Money(10, "EUR");
 let twentyEuros = tenEuros.times(2)
 assert.strictEqual(twentyEuros.amount, 20);
