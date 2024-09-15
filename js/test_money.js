8a6b911e6f40e61d7ab9364b5511adb1ccd86256
bangjuyoung <juyoungbang82@gmail.com>
2024-09-15 18:01:05 +0900

    feat(chap03): red test

--- js/test_money.js
+++ js/test_money.js
@@ -27,3 +27,8 @@ let originalMoney = new Money(4002, "KRW");
 let actualMoneyAfterDivision = originalMoney.divide(4);
 let expectedMoneyAfterDivision = new Money(1000.5, "KRW");
 assert.deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivision);
+
+let fifteenDollars = new Money(15, "USD");
+let portfolio = new Portfolio();
+portfolio.add(fiveDollars, tenDollars);
+assert.deepStrictEqual(portfolio.evaluate("USD"), fifteenDollars);
