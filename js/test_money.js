acb61dd67b7d0106dbde7021215dfedfaf678c19
bangjuyoung <juyoungbang82@gmail.com>
2024-09-15 19:55:07 +0900

    feat(chap06): adding a test class and test methods

--- js/test_money.js
+++ js/test_money.js
@@ -2,18 +2,34 @@ const assert = require('assert');
 const Money = require("./money");
 const Portfolio = require("./portfolio");
 
-let tenEuros = new Money(10, "EUR");
-let twentyEuros = new Money(20, "EUR");
-assert.deepStrictEqual(tenEuros.times(2), twentyEuros);
+class MoneyTest {
+    testMultiplication() {
+        let tenEuros = new Money(10, "EUR");
+        let twentyEuros = new Money(20, "EUR");
+        assert.deepStrictEqual(tenEuros.times(2), twentyEuros);
+    }
 
-let originalMoney = new Money(4002, "KRW");
-let actualMoneyAfterDivision = originalMoney.divide(4);
-let expectedMoneyAfterDivision = new Money(1000.5, "KRW");
-assert.deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivision);
+    testDivision() {
+        let originalMoney = new Money(4002, "KRW");
+        let actualMoneyAfterDivision = originalMoney.divide(4);
+        let expectedMoneyAfterDivision = new Money(1000.5, "KRW");
+        assert.deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivision);
+    }
 
-let fiveDollars = new Money(5, "USD");
-let tenDollars = new Money(10, "USD");
-let fifteenDollars = new Money(15, "USD");
-let portfolio = new Portfolio();
-portfolio.add(fiveDollars, tenDollars);
-assert.deepStrictEqual(portfolio.evaluate("USD"), fifteenDollars);
+    testAddition() {
+        let fiveDollars = new Money(5, "USD");
+        let tenDollars = new Money(10, "USD");
+        let fifteenDollars = new Money(15, "USD");
+        let portfolio = new Portfolio();
+        portfolio.add(fiveDollars, tenDollars);
+        assert.deepStrictEqual(portfolio.evaluate("USD"), fifteenDollars);
+    }
+
+    runAllTests() {
+        this.testMultiplication();
+        this.testDivision();
+        this.testAddition();
+    }
+}
+
+new MoneyTest().runAllTests();
